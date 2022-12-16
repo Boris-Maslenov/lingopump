edf55cda6a884a4f5be6654cbb0733855946a300
Boris-Maslenov <ya.maslenov@ya.ru>
2022-12-16 10:21:42 +0300

    add friends section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './index.html';
+import './modules/plyr/plyr.css';
+import * as Plyr from './modules/plyr/plyr.min';
+import Swiper from 'swiper';
+import 'swiper/css';
 import './scss/app.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const stepPlayer = new Plyr('audio', {
+        settings: [],
+    });
+
+    const swiper = new Swiper('.select-friend', {
+        slidesPerView: 'auto',
+        // centeredSlides: true,
+        spaceBetween: 60,
+        // freeMode: true,
+    });
+
 });
